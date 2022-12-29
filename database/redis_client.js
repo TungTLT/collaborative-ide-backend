851c146cf16eaf54daec918bde57d5131e997acd
TungTLT <tranlethanhtung554@gmail.com>
2022-12-29 22:19:27 +0700

    add config for redis server

--- database/redis_client.js
+++ database/redis_client.js
@@ -2,7 +2,7 @@ const { createClient } = require('redis')
 const { redBright } = require('chalk')
 const REDIS_EVENT = require('../utils/constants')
 
-const redisClient = createClient()
+const redisClient = createClient({ url: 'redis://red-cemqscun6mpoovjkk180:6379' })
 // redis event handler
 redisClient.on(REDIS_EVENT.ERROR, (e) => console.log(redBright.bold(`ERROR connecting to Redis ${e}`)))
 
