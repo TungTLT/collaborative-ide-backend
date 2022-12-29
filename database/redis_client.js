1516cf06580975986e6488d42ce7befe02ac2030
TungTLT <tranlethanhtung554@gmail.com>
2022-12-29 22:44:29 +0700

    add redis server url

--- database/redis_client.js
+++ database/redis_client.js
@@ -1,8 +1,9 @@
 const { createClient } = require('redis')
 const { redBright } = require('chalk')
 const REDIS_EVENT = require('../utils/constants')
+require('dotenv').config()
 
-const redisClient = createClient({ url: 'redis://red-cemqscun6mpoovjkk180:6379' })
+const redisClient = createClient({ url: process.env.REDIS_SERVER_URL })
 // redis event handler
 redisClient.on(REDIS_EVENT.ERROR, (e) => console.log(redBright.bold(`ERROR connecting to Redis ${e}`)))
 
