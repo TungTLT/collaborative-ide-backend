d404db4ac3d241887376f181bdcb3d719df4c59f
TungTLT <tranlethanhtung554@gmail.com>
2023-01-11 23:20:35 +0700

    change for deploy

--- database/redis_client.js
+++ database/redis_client.js
@@ -3,8 +3,8 @@ const { redBright } = require('chalk')
 const { REDIS_EVENT } = require('../utils/constants')
 require('dotenv').config()
 
-// const redisClient = createClient({ url: process.env.REDIS_SERVER_URL })
-const redisClient = createClient()
+const redisClient = createClient({ url: process.env.REDIS_SERVER_URL })
+// const redisClient = createClient()
 
 // redis event handler
 redisClient.on(REDIS_EVENT.ERROR, (e) => console.log(redBright.bold(`ERROR connecting to Redis ${e}`)))
